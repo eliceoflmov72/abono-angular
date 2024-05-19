750a74b441efa16ff0bd59f51bbbf01be0d04bfb
eliceoflmov72 <eliceomoreta@gmail.com>
2024-05-19 17:59:37 +0200

    Terminado el desarrollo genérico del mapa. Reajustados algunos componentes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 // Importamos los componentes que hemos creado en esta aplicación.
 import { HomeComponent } from './pages/home/home.component';
+import { MapComponent } from './pages/map/map.component';
 
 // Importamos las rutas correspondientes
 export const routes: Routes = [
+    { path: '', component: HomeComponent, title: 'Inicio'},
+    { path: 'Mapa', component: MapComponent, title: 'Mapa' },
     { path: '**',redirectTo: '', pathMatch: 'full' },
-    { path: '', component: HomeComponent, title: 'Inicio'}
 ];
--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.scss'
+  styleUrls: ['./map.component.scss']
 })
-export class MapComponent {
 
+export class MapComponent implements AfterViewInit {
+  private map ?: L.Map;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      attributionControl: false,
+      center: [ 40.416775, -3.70379 ],
+      zoom: 6
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
 }
